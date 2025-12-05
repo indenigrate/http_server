6e03175b4941cd0958f2936c398b7284fe885a20
Devansh Soni <devanshsoni899@gmail.com>
2025-12-06 01:11:33 +0530

    Sending response 200 and 404 based on url

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -16,21 +17,51 @@ func main() {
 
 	// TODO: Uncomment the code below to pass the first stage
 	//
-
+	// Listen at 4221
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	fmt.Println("Port 4221 Binded ")
 
+	// Accept connection
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-
 	fmt.Println("Connection Accepted from ", conn.RemoteAddr())
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
+	// Respond with a simple HTTP response
+	handleConnection(conn, "/")
+	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+}
+
+// handle connection function
+func handleConnection(conn net.Conn, targetURL string) {
+	defer conn.Close()
+	b := make([]byte, 1024)
+
+	_, err := conn.Read(b)
+	if err != nil {
+		fmt.Println("Error reading response: ", err.Error())
+		return
+	}
+
+	req := string(b)
+	reqParts := strings.Split(req, "\r\n")
+	// reqParts[0] is the request line
+	reqLineParts := strings.Split(reqParts[0], " ")
+	// method := reqLineParts[0]
+
+	target := reqLineParts[1]
+	if target == targetURL {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	return
 }
