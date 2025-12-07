77296082d95a859d1dc646ac12d23d4a6f967d5f
Devansh Soni <devanshsoni899@gmail.com>
2025-12-07 19:49:11 +0530

    Creating file via POST req in a directory

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
@@ -56,7 +57,7 @@ func handleConnection(conn net.Conn, dir string) {
 	// reqParts[0] is the request line
 	// reqParts[1] is the first header line
 	// reqParts[2] is the second header line
-
+	reqBody := reqParts[len(reqParts)-1]
 	reqLineParts := strings.Split(reqParts[0], " ")
 	// method := reqLineParts[0]
 
@@ -85,16 +86,40 @@ func handleConnection(conn net.Conn, dir string) {
 		}
 	} else if targets[1] == "files" {
 		fmt.Println("serving /files/{filename}")
-		// handle /echo/{filename}
-
-		file, err := os.ReadFile(dir + "/" + targets[2])
-		if err != nil {
-			fmt.Println("Error opening file:", err)
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			return
+		// handle /files/{filename}
+		filename := targets[2]
+		fileAddr := dir + "/" + filename
+		// handle GET
+		if reqLineParts[0] == "GET" {
+			file, err := os.ReadFile(fileAddr)
+			if err != nil {
+				fmt.Println("Error opening file:", err)
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				return
+			}
+			content := string(file)
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
+		} else if reqLineParts[0] == "POST" {
+			// handle POST
+			err := os.MkdirAll(dir, 0755) // 0755 grants read/write/execute for owner, read/execute for group/others
+			if err != nil {
+				log.Fatalf("Failed to create directory: %v", err)
+			}
+			file, err := os.Create(fileAddr)
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+				return
+			}
+			defer file.Close()
+			_, err = file.WriteString(reqBody)
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+				return
+			}
+			conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
 		}
-		content := string(file)
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
